7902898fd70c015e167367b85ef7192100f9d419
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2020-08-23 17:51:34 -0300

    :seedling: Added admin seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(Sellers::class);
         $this->call(EstatesForTest::class); //JUST FOR LOCAL TEST
         $this->call(EmployeeForTest::class); //JUST FOR LOCAL TEST
+        $this->call(UserForTest::class); //JUST FOR LOCAL TEST
     }
 }
